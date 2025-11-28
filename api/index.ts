0bc9eda3b227741d9671b6590323787e0eb53323
mk2leo <72913711+mk2leo@users.noreply.github.com>
2025-11-28 18:49:08 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,3 @@
 // This file acts as a simple entry point for Vercel to find the Serverless Function.
 // It imports and executes the main server logic from the 'server' directory.
-import "../server/_core/index";
+import "../server/_core/index.js";
